a16e73c9d2190b6f52a0bbe12366652de74607e1
pdombek <patryk.dombek@gmail.com>
2019-04-20 02:49:57 +0200

    Third commit - 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int resultAdd = 4+5;
+        int resultSubtract = 10-7;
+        if (resultAdd==(calculator.add(4,5))){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (resultSubtract==(calculator.subtract(10,7))){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
