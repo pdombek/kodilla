9322deb015d75d382b607d99bb1a9ab3abcefd90
pdombek <patryk.dombek@gmail.com>
2019-06-10 22:04:30 +0200

    7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,16 @@
 package com.kodilla.stream;
 
+import java.util.*;
+import java.lang.*;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
@@ -18,5 +23,11 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Making text more beautiful");
+        poemBeautifier.beautify("Testujemy", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("Testujemy", (text) -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("Testujemy", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("Testujemy", (text) -> text.replaceAll(text,"PIĘKNIEJSZY"));
     }
 }
