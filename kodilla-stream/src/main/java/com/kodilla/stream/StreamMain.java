fe6868d84901486d3942a195ac5d416122f949a1
pdombek <patryk.dombek@gmail.com>
2019-06-12 01:03:02 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.lang.*;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -29,5 +30,8 @@ public class StreamMain {
         poemBeautifier.beautify("Testujemy", (text) -> "ABC" + text + "ABC");
         poemBeautifier.beautify("Testujemy", (text) -> text.toLowerCase());
         poemBeautifier.beautify("Testujemy", (text) -> text.replaceAll(text,"PIĘKNIEJSZY"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
