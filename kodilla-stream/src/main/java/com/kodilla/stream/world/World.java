71bb4a198b2e554681cc7ce35dc219f335d05b08
pdombek <patryk.dombek@gmail.com>
2019-06-21 16:37:28 +0200

    7.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+    private final List<Continent> continents = new ArrayList<>();
+
+    public void addContinent(Continent continent) {
+        continents.add(continent);
+    }
+
+    public List<Continent> getContinents() {
+        return continents;
+    }
+
+    public BigDecimal getPeopleQuantity(){
+        BigDecimal allPeople = continents.stream()
+                .flatMap(continent -> continent.getCountries().stream())
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+        return allPeople;
+    }
+}
